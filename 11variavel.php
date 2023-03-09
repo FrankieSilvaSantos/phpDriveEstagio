51169606e146fbb619f971a14c812f6ae57d9fbe
Frankie Silva Santos <109253810+FrankieSilvaSantos@users.noreply.github.com>
2023-03-09 10:38:50 -0300

    Add files via upload

---
+++ 11variavel.php
@@ -0,0 +1,16 @@
+<?php
+
+$genero = 'N';
+$nomeCompleto = 'Frankie Yoogan';
+$salario = 3.400;
+$valido = true;
+
+print(gettype($nomeCompleto));
+print "\n";
+print(gettype($genero));
+print "\n";
+print (gettype($salario));
+print "\n";
+print (gettype($valido));
+
+?>
