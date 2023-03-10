31aa059675653b8cb46e8e5149833e5759f6cb0a
Frankie Silva Santos <109253810+FrankieSilvaSantos@users.noreply.github.com>
2023-03-10 15:16:17 -0300

    Add files via upload

---
+++ 15switch.php
@@ -0,0 +1,25 @@
+<?php
+
+$dia = 'quint';
+
+switch ($dia) {
+    case 'seg':
+        echo "Aula de matematica";
+        break;
+        case 'terc':
+            echo "Aula de portugues";
+            break;
+            case 'quart':
+                echo "Aula de ciencias";
+                break;
+                case 'quint':
+                    echo "Aula de biologia";
+                    break;
+                    case 'sex':
+                        echo "Aula de educacao fisica";
+                        break;
+                        default:
+                        echo "Nao é um dia valido";
+}
+
+?>
