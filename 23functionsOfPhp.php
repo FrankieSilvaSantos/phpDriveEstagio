68531be31e343d7a98360638fc9da84076032eaf
Frankie Silva Santos <109253810+FrankieSilvaSantos@users.noreply.github.com>
2023-03-20 15:54:48 -0300

    Add files via upload

--- 23functionsOfPhp.php
+++ 23functionsOfPhp.php
@@ -4,6 +4,26 @@ $nome = "Franzie Yoogan";
 
 echo substr($nome,0,4);
 
-echo strtolower("\n SHEEESH!");
+echo strtolower("\n SHEEESH! \n");
+
+echo strtoupper($nome);
+
+$msg = "<h1 style='color:red;'> Olá me consagrade </h1> <br> <a href='www.google.com.br'>LINK </a> <br>";
+
+echo $msg;
+
+$string = "maça,uva,laranja,banana";
+
+$alterar = str_replace("maça","tangerina",$string);
+
+echo "\n {$alterar} \n";
+
+$nome2 = "Frankie Yoogan";
+
+if(strlen($nome2) <= 22) {
+    echo " <br> O nome e: {$nome2}";
+} else {
+    echo " <br> nome invalido!!";
+}
 
 ?>
